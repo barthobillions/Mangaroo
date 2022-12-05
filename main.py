6563e3d96b78ead24cbf9fbcb7fb73daf5d41be7
Nhat <58672082+gnatttt@users.noreply.github.com>
2022-12-05 00:54:45 -0500

    working on support for updating when run

--- main.py
+++ main.py
@@ -2,10 +2,22 @@ import reader, downloader
 
 if __name__ == "__main__":
 	print("================= SELECT PROGRAM ===================")
-	print("(0) - DOWNLOADER")
-	print("(1) - READER")
+	print("                 *ENTER A NUMBER*")
+	print("(1) - DOWNLOADER")
+	print("(2) - READER")
 	choice = input("> ")
-	if choice == '0':
-		downloader.download_controller()
+	if choice == '1':
+		# Tries to create parent directory. Will be made if not existing
+		# If does not exist, 'Material' directory will be made
+		print("Creating parent directory...")
+		time.sleep(.5)
+		try:
+			os.mkdir("Material")
+		except:
+			print("'Material' directory already exists.")
+			time.sleep(.5)
+		name, link = downloader.download_controller()
+		with open("Material/" + name +"/data.txt", "w") as writer:
+			writer.write(link)
 	else:
 		reader.control_loop()
