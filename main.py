f3a41d7d5afdb034a747feba00e6ba99a9e24682
Nhat <58672082+gnatttt@users.noreply.github.com>
2023-01-04 00:46:29 -0500

    removed creation of data.txt and updated menu

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
 import reader, downloader
+import os
 import time
 
 if __name__ == "__main__":
-	print("================= SELECT PROGRAM ===================")
-	print("                 *ENTER A NUMBER*")
-	print("(1) - DOWNLOADER")
-	print("(2) - READER")
-	print("(q) - QUIT")
-	choice = input("> ")
-	if choice == '1':
-		# Tries to create parent directory. Will be made if not existing
-		# If does not exist, 'Material' directory will be made
-		print("Creating parent directory...")
-		time.sleep(.5)
-		try:
-			os.mkdir("Material")
-		except:
-			print("'Material' directory already exists.")
-		# download_controller() returns the name and link of the manga
-		name, link = downloader.download_controller()
-		# data.txt file is created to store this information to be used
-		with open("Material/" + name +"/data.txt", "w") as writer:
-			writer.write("0\n")
-			writer.write(link)
-	elif choice == "2":
-		reader.control_loop()
-	else:
-		print("--- QUITTING ---")
+	# Tries to create parent directory. Will be made if not existing
+	# If does not exist, 'Material' directory will be made
+	print("Creating parent directory...")
+	time.sleep(.8)
+	try:
+		os.mkdir("Material")
+		print("No existing material found, created parent directory located at 'Material'")
+	except:
+		print("'Material' directory already exists.")
+	time.sleep(.8)
+	run = True
+	while run:
+		os.system('cls')
+		print("================= SELECT PROGRAM ===================")
+		print("                 *ENTER A NUMBER*")
+		print("(1) - DOWNLOADER")
+		print("(2) - READER")
+		print("(x) - QUIT")
+		choice = input("> ")
+		if choice == '1':
+			# download_controller() returns the name and link of the manga
+			downloader.download_controller()
+		elif choice == "2":
+			reader.control_loop()
+		elif choice == "x":
+			print("--- QUITTING ---")
+			time.sleep(1)
+			run = False
+		else:
+			print("Invalid choice...")
+			time.sleep(.7)
